244288b7c3f9311c48a6e97b560f44f74014d5ef
tangbc_ <15760666366@163.com>
2026-01-08 18:46:59 +0800

    1 commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts（TS 项目）
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    // 关键：配置 @ 别名指向 src 目录
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
